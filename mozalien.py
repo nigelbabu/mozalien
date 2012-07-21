3952a9f0911b9985cf15be02f89be28cd809b7b0
Nigel Babu <nigel@hasgeek.com>
2012-07-21 21:59:58 +0530

    Now I can submit!

--- mozalien.py
+++ mozalien.py
@@ -6,12 +6,19 @@ import json
 def login(reddit):
     with open('settings.json') as f:
         json_data = f.read()
+        print json_data
         settings = json.loads(json_data)
         reddit.login(settings['username'], settings['password'])
 
 
+def submit(reddit, subreddit, url, title):
+    sub = r.get_subreddit(subreddit)
+    sub.submit(title=title, url=url)
+
+
 def main():
     r = praw.Reddit(user_agent='mozalien by /u/nigelbabu')
+    login(r)
     submissions = r.get_subreddit('mozillamemes').get_hot(limit=5)
     print [str(x) for x in submissions]
 
